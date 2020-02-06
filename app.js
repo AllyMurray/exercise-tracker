173d6269ae946344b755fc9989527f1672271ccb
Ally Murray <allymurray88@gmail.com>
2020-02-06 19:10:03 +0000

    Added body parser and configured to parse form data

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const cors = require('cors');
 const express = require('express');
+const bodyParser = require('body-parser');
 const exerciseRouter = require('./controllers/exercise');
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', (request, response) => {
