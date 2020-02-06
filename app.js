30fd700087a8efd01c18713c0cb93fcc9d68b933
Ally Murray <allymurray88@gmail.com>
2020-02-06 19:59:50 +0000

    Mongoose added and configured

--- app.js
+++ app.js
@@ -2,7 +2,24 @@ const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
 const exerciseRouter = require('./controllers/exercise');
+const mongoose = require('mongoose');
+const config = require('./utils/config');
+const logger = require('./utils/logger');
 const app = express();
+
+logger.info('connecting to:', config.MONGODB_URI);
+
+mongoose
+  .connect(config.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    logger.info('connected to MongoDB');
+  })
+  .catch(error => {
+    logger.error('error connecting to MongoDB:', error.message);
+  });
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
